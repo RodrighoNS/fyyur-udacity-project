ee20682878340ee31f2ec77418384fd10ccdf1ee
RodrighoNS <rnavarro@lemontech.com>
2021-08-29 21:21:58 -0400

    Add models file

--- models.py
+++ models.py
@@ -36,8 +36,8 @@ class Show(db.Model):
     __tablename__ = 'Show'
 
     id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=True)
+    start_time = db.Column(db.DateTime)
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
